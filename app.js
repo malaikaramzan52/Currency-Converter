62dc67eb69bd393fd38d2073b2538f55e3d65cb5
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-09-09 03:13:54 -0700

    Currency Converter

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const BASE_URL=
-  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 const dropdown = document.querySelectorAll(".Selects");
 const btn = document.getElementById("btn");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
+const msg = document.getElementById("msg");
 let i = 0;
+window.addEventListener("load", () => {
+    updateExchangeRate();
+});
+
+
 //country list
 for (let select of dropdown) {
     for (currCode in countryList) {
@@ -23,8 +28,26 @@ for (let select of dropdown) {
     }
     select.addEventListener("change", (evt) => {
         updateFlag(evt.target);
-    })
+    });
 }
+    const updateExchangeRate = async () => {
+        let amount = document.querySelector(".amount input");
+        let amVal = amount.value;
+
+        if (amVal.trim() === "" || amVal < 1) {
+            amVal = 1;
+            amount.value = "1";
+        }
+
+        const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+        let response = await fetch(URL);
+        let data = await response.json();
+        rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+        // console.log(rate);
+        let finalAmount = (amVal * rate);
+        msg.innerText = `${amVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+    }
+
 const updateFlag = (element) => {
     let currCode = element.value;
     let countryCode = countryList[currCode];
@@ -32,17 +55,8 @@ const updateFlag = (element) => {
     let img = element.parentElement.querySelector("img");
     img.src = newSrc;
 };
-btn.addEventListener("click", async (evt) => {
+btn.addEventListener("click", (evt) => {
     evt.preventDefault();
-    let amount = document.querySelector(".amount input");
-    let amVal = amount.value;
-    if (amVal === " " || amVal < 1) {
-        amVal = 1;
-        amount.value = "1";
-    }
-    const URL = `${BASE_URL}/${fromCurr.value.toLowerCae()}/${toCurr.value.toLowerCase()}.json`;
-    let response = await fetch(URL);
-    let data= await response.json();
-    console.log(response);
-
+    updateExchangeRate();
 });
+
